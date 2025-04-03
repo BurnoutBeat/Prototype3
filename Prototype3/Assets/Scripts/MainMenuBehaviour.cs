e5fdc1e95b047b12bd92c4b67c078a95a54d6888
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2025-04-03 15:48:43 -0500

    Start loads the game scene

--- Prototype3/Assets/Scripts/MainMenuBehaviour.cs
+++ Prototype3/Assets/Scripts/MainMenuBehaviour.cs
@@ -1,18 +1,40 @@
+/******************************************************************************
+ * Author: Brad Dixon
+ * File Name: MainMenuBehaviour.cs
+ * Creation Date: 4/3/2025
+ * Brief: Contains code for menu buttons
+ * ***************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string sceneName;
+
+    /// <summary>
+    /// Loads the game scene
+    /// </summary>
+    public void LoadGameScene()
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Quits out of the game
+    /// </summary>
+    public void QuitGame()
     {
-        
+        //Quits out of the editor instead
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
     }
 }
