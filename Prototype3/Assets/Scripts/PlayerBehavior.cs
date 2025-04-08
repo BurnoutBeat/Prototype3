d9de72145108c7f7dfdbfd7dac677f1351832e7f
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2025-04-08 16:46:35 -0500

    sliddy

--- Prototype3/Assets/Scripts/PlayerBehavior.cs
+++ Prototype3/Assets/Scripts/PlayerBehavior.cs
@@ -12,6 +12,7 @@ public class PlayerBehavior : MonoBehaviour
     private Vector2 moveInput;
     private Vector2 lookDelta;
     private Rigidbody rb;
+    private CapsuleCollider capsuleCollider;
     private float verticalRotation = 0f;
 
     public GameObject eyes; //camera
@@ -21,14 +22,17 @@ public class PlayerBehavior : MonoBehaviour
     public float crouchChargeTime = 2f;
     public float maxCrouchJumpPower = 10f;
     public float moveSpeed = 5f;
+    public float airMoveSpeed = 2.5f;
+    
     public float rotationSpeed = 5f;
-    public float crouchSpeed = 1f;
-    [SerializeField] float dashPower = 100f;
-    [SerializeField] float groundDashCooldown = 1f;
+    public float dashPower = 100f;
+    public float groundDashCooldown = 1f;
     [Tooltip("Set between 0-1")]
-    [SerializeField] float groundDashReduction = 0.75f;
+    public float groundDashReduction = 0.75f;
 
-    public bool crouching;
+    private bool crouching = false;
+    private bool crouchingMovment = false;
+    private bool capsLockHeld = false;
     private float crouchStartTime;
 
     private PlayerAbilities playerAbilities;
@@ -38,6 +42,7 @@ public class PlayerBehavior : MonoBehaviour
 
     private void Awake()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         inputActions = new PlayerControls();
@@ -83,25 +88,25 @@ public class PlayerBehavior : MonoBehaviour
     }
     private void OnJump(InputAction.CallbackContext context)
     {
-        if (grounded() && crouching)
+        if (grounded() && (crouching && !crouchingMovment))
         {
             rb.AddForce(Vector3.up * (jumpForce + (maxCrouchJumpPower * chargeStrength)), ForceMode.Impulse);
         }
-        else if (grounded()) 
+        else if (grounded() && !crouchingMovment) 
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-        if (crouching) {
-            crouching = false;
-            Vector3 newPos = transform.position;
-            newPos.y += 0.5f;
-            eyes.transform.position = newPos;
+        if (crouching && !crouchingMovment) {
+            StandUp();
         }
     }
     private void CrouchPerformed(InputAction.CallbackContext context)
     {
+        capsLockHeld = true;
         crouchStartTime = Time.time;
         if (grounded()) {
+            capsuleCollider.height = 1;
+            capsuleCollider.center = new Vector3(0,-0.5f, 0);
             crouching = true;
             Vector3 newPos = transform.position;
             newPos.y -= 0.5f;
@@ -110,15 +115,25 @@ public class PlayerBehavior : MonoBehaviour
     }
     private void CrouchCancled(InputAction.CallbackContext context)
     {
-        if (crouching) {
-            crouching = false;
-            Vector3 newPos = transform.position;
-            newPos.y += 0.5f;
-            eyes.transform.position = newPos;
-            rb.velocity = Vector2.zero;
+        capsLockHeld = false;
+        if (crouching && CanUncrouch()) {
+            StandUp();
         }  
+        else if (crouching)
+        {
+            crouchingMovment = true;
+        }
     }
-
+    private void StandUp() {
+        capsuleCollider.height = 2;
+        capsuleCollider.center = Vector3.zero;
+        crouching = false;
+        crouchingMovment = false;
+        Vector3 newPos = transform.position;
+        newPos.y += 0.5f;
+        eyes.transform.position = newPos;
+        rb.velocity = Vector2.zero;
+    }
     private void OnDash(InputAction.CallbackContext ctx)
     {
         if(grounded() && canDashGround)
@@ -133,9 +148,23 @@ public class PlayerBehavior : MonoBehaviour
             canDashAir = false;
         }
     }
+    private bool CanUncrouch() {
+        float rayDistance = 0.6f;
+        Vector3 BoxSize = new Vector3(0.4f, 0.5f, 0.4f);
+        Vector3 centerPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        RaycastHit[] hits = Physics.BoxCastAll(centerPos, BoxSize, Vector3.up, Quaternion.identity, rayDistance);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject != gameObject)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     private bool grounded() {
         float rayDistance = 0.6f;
-        Vector3 BoxSize = new Vector3(0.5f, 0.5f, 0.5f);
+        Vector3 BoxSize = new Vector3(0.4f, 0.5f, 0.4f);
         Vector3 centerPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         RaycastHit[] hits = Physics.BoxCastAll(centerPos, BoxSize, Vector3.down, Quaternion.identity, rayDistance);
         foreach (RaycastHit hit in hits)
@@ -152,19 +181,25 @@ public class PlayerBehavior : MonoBehaviour
     {
         Vector3 moveDirection = (transform.forward * moveInput.y) + (transform.right * moveInput.x);
         moveDirection.Normalize();
-
+        if (CanUncrouch() && !capsLockHeld && crouching)
+        {
+            StandUp();
+        }
         if (crouching) {
             Vector3 flatVelocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
             if (flatVelocity.magnitude > moveSpeed)
             {
                 rb.velocity = new Vector3(flatVelocity.normalized.x * moveSpeed, rb.velocity.y, flatVelocity.normalized.z * moveSpeed);
             }
+            if (crouchingMovment) {
+                rb.AddForce(moveDirection * moveSpeed * 40f * Time.fixedDeltaTime);
+            }
         } else {
             
             if (grounded()) {
                 rb.AddForce(moveDirection * moveSpeed * 100f * Time.fixedDeltaTime);
             } else {
-                rb.AddForce(moveDirection * moveSpeed * 20f * Time.fixedDeltaTime);
+                rb.AddForce(moveDirection * airMoveSpeed * 100f * Time.fixedDeltaTime);
             } 
             Vector3 flatVelocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
             if (flatVelocity.magnitude > moveSpeed)
