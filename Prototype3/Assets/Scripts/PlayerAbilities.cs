1bbe493cbfe7ed0a3e83562cb450758f3da8f8a1
Veizyl <sturner@mail.bradley.edu>
2025-04-10 14:05:16 -0500

    Update PlayerAbilities.cs

--- Prototype3/Assets/Scripts/PlayerAbilities.cs
+++ Prototype3/Assets/Scripts/PlayerAbilities.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerAbilities : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] GameObject player;
+    [SerializeField] float hangTime = 1f;
 
     public void Dash(float power)
     {
+        rb.useGravity = false;
         rb.AddForce(player.transform.forward * power, ForceMode.Force);
+
+        StartCoroutine(DashStop());
+    }
+
+    private IEnumerator DashStop()
+    {
+        yield return new WaitForSeconds(hangTime);
+
+        rb.useGravity = true;
     }
 }
