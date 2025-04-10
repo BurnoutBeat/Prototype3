9c370a8c58686a71d22c84710c272b6b623d88ce
Veizyl <sturner@mail.bradley.edu>
2025-04-10 14:05:52 -0500

    reset PlayerAbilities to main

--- Prototype3/Assets/Scripts/PlayerAbilities.cs
+++ Prototype3/Assets/Scripts/PlayerAbilities.cs
@@ -5,20 +5,9 @@ public class PlayerAbilities : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] GameObject player;
-    [SerializeField] float hangTime = 1f;
 
     public void Dash(float power)
     {
-        rb.useGravity = false;
         rb.AddForce(player.transform.forward * power, ForceMode.Force);
-
-        StartCoroutine(DashStop());
-    }
-
-    private IEnumerator DashStop()
-    {
-        yield return new WaitForSeconds(hangTime);
-
-        rb.useGravity = true;
     }
 }
